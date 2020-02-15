bd6ab6437994b795f146fe35d2b836aad67066a4
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2020-02-15 04:51:36 +0200

    minor clean up on the frist part of the project

--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -4,7 +4,10 @@ public class Die {
     public static void main(String[] args) {
         Die die6 = new Die(6);
         die6.roll();
+        Die die20 = new Die(20);
+        die20.roll();
         System.out.println(die6.value);
+        System.out.println(die20.value);
 
     }
     private int sides;
@@ -13,17 +16,16 @@ public class Die {
     public Die(int sides) {
         this.sides = sides;
     }
-    public void setSides(int sides) {
-        if(sides > 1){
-            this.sides = sides;
-        }else {
-            throw new IllegalArgumentException("Sides should be greater than 1");
-        }
-    }
+//    public void setSides(int sides) {
+//        if(sides > 1){
+//            this.sides = sides;
+//        }else {
+//            throw new IllegalArgumentException("Sides should be greater than 1");
+//        }
+//    }
 
-    public int roll(){
+    public void roll(){
         Random random = new Random();
         value = Math.abs(random.nextInt()%sides) + 1;
-        return value;
     }
 }
